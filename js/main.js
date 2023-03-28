448e05ad51b67508e1a0ffc2a1a109f052c44aef
visualtuberu <visualtuberu@gmail.com>
2023-03-28 22:28:51 +0300

    fix enter add new task

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
 const newTask = document.querySelector('.newTask'),
-	newTaskBtn = document.querySelector('.addTaskBtn'),
-	todosWrapper = document.querySelector('.todos-wrapper');
+  newTaskBtn = document.querySelector('.addTaskBtn'),
+  todosWrapper = document.querySelector('.todos-wrapper');
 
 newTaskBtn.disabled = true;
 newTaskBtn.addEventListener('click', () => {
-	addNewTask();
+  addNewTask();
   reCountTasks();
 
 })
 
-newTask.addEventListener('input', (event)=>{
-  
+newTask.addEventListener('input', (event) => {
+
   if (newTask.value.trim() != '') newTaskBtn.disabled = false;
   else newTaskBtn.disabled = true;
+    
 })
 
+newTask.addEventListener('keydown', (event)=>{
+  if (event.code == 'Enter' && newTaskBtn.disabled == false) {
+    addNewTask();
+    reCountTasks(); 
+  }
+})
 
 todosWrapper.addEventListener('click', (event) => {
 
-	if (event.target.classList.contains('del')) {
-		event.target.closest('.todo-item').remove();
-		reCountTasks();
-	}
-	if (event.target.classList.contains('done')) {
-		if (event.target.value == 'Выполнено') {
-			event.target.closest('.todo-item').classList.add('done-ok');
-			event.target.value = 'Не выполнено';
-		}
-		else if (event.target.value == 'Не выполнено') {
-			event.target.closest('.todo-item').classList.remove('done-ok');
-			event.target.value = 'Выполнено';
-		}
-	}
+  if (event.target.classList.contains('del')) {
+    event.target.closest('.todo-item').remove();
+    reCountTasks();
+  }
+  if (event.target.classList.contains('done')) {
+    if (event.target.value == 'Выполнено') {
+      event.target.closest('.todo-item').classList.add('done-ok');
+      event.target.value = 'Не выполнено';
+    }
+    else if (event.target.value == 'Не выполнено') {
+      event.target.closest('.todo-item').classList.remove('done-ok');
+      event.target.value = 'Выполнено';
+    }
+  }
 
 })
 
@@ -39,11 +46,11 @@ todosWrapper.addEventListener('click', (event) => {
 
 function addNewTask() {
 
-	let userTask = newTask.value;
-	let taskDiv = document.createElement('div');
+  let userTask = newTask.value;
+  let taskDiv = document.createElement('div');
 
-	taskDiv.classList.add('todo-item');
-	taskDiv.innerHTML = `
+  taskDiv.classList.add('todo-item');
+  taskDiv.innerHTML = `
 		  <div class="task-wrapper"> 
 			<span class="task-counter"></span>
 			<span class="task">${userTask}</span>
@@ -53,21 +60,21 @@ function addNewTask() {
           	<input class="del" type="button" value="Удалить">
 		  </span>
 	`;
-	todosWrapper.prepend(taskDiv);
-	newTask.value = '';
+  todosWrapper.prepend(taskDiv);
+  newTask.value = '';
   newTaskBtn.disabled = true;
 
 };
 
 function reCountTasks() {
-	let todoArr = document.querySelectorAll('.task-counter');
-	console.log(todoArr);
-	let counter = todoArr.length;
-	todoArr.forEach(elem => {
-		elem.innerHTML = counter--;
-		
-	});
+  let todoArr = document.querySelectorAll('.task-counter');
+  console.log(todoArr);
+  let counter = todoArr.length;
+  todoArr.forEach(elem => {
+    elem.innerHTML = counter--;
+
+  });
 }
 function checkEmptyNewTaskInput() {
-  
+
 }
